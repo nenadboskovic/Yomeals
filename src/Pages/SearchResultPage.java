230694ecde4fbf54e455d9cb026a092357a0993c
unknown <nenadboskovic1@gmail.com>
2021-06-21 20:09:49 +0200

    Search result page update

--- src/Pages/SearchResultPage.java
+++ src/Pages/SearchResultPage.java
@@ -1,7 +1,12 @@
 package Pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchResultPage extends BasicPage{
@@ -11,6 +16,21 @@ public class SearchResultPage extends BasicPage{
 
 	}
 
+	public List<WebElement> getSearchResults() {
+		List<WebElement> resultLinks = driver.findElements(By.xpath("//*[@class='product-name']/a"));
+		return resultLinks;
+	}
 	
+	public ArrayList<String> getSearchResultsText() {
+		ArrayList<String> textList = new ArrayList<String>();
+		for (int i = 0; i < this.getSearchResults().size(); i++) {
+			textList.add(this.getSearchResults().get(i).getText());
+		}
+		return textList;
+	}
+	
+	public int getSearchResultsNum() {
+		return this.getSearchResults().size();
+	}
 	
 }
